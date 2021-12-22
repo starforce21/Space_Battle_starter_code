b07d96f69721f2d111d9294b760b5100d58ffde2
starforce21 <jackjack1214@gmail.com>
2021-12-21 23:13:40 -0500

    done for tonight

--- app.js
+++ app.js
@@ -1,78 +1,148 @@
-let yourHull=20
-let yourFirePower=5
-let yourAccuracy=.7
-let enemyCount=6
 const battleLoop=()=>{
     while(enemyHull>0 && yourHull>0){
-        alert(`Your current HP=${yourHull}. Enemy current HP=${enemyHull}`)
+        console.log(`Your current HP=${yourHull} and Shield=${yourShield}. Enemy current HP=${enemyHull}`)
         if(didYouHit(yourAccuracy)){
             enemyHull-=yourFirePower
-            alert(`You Hit and dealt 5dmg. Enemy now have ${enemyHull}hp`)
+            console.log(`You Hit and dealt ${yourFirePower}dmg. Enemy now have ${enemyHull}hp`)
             if(enemyHull<1){
-                alert('Enemy is dead')
+                console.log('Enemy is dead, You gain one pp')
+                shieldRegen()
+                powerPoints++
+                level++
                 enemyCount--
             }
             else {
+                console.log('Enemy survived and is attempting to fire at you')
                 if(didYouHit(enemyAccuracy)){
-                    alert('Enemy surviving and is attempting to hit you')
-                    yourHull-=enemyFirePower
+                    console.log(`Enemy hit you for ${enemyFirePower}`)
+                    dmgCalc()
                     if(yourHull<1){
-                        alert('Enemy breached your hull and you are dead')
-
+                        console.log('Enemy breached your hull and you are dead')
                     }
                 }
             }    
         }
         else{
-            alert('You missed your shot. Enemy is attacking')
+            console.log('You missed your shot. Enemy is attacking')
             if(didYouHit(enemyAccuracy)){
-                alert(`Enemy hit you for ${enemyFirePower}`)
-                yourHull-=enemyFirePower
+                console.log(`Enemy hit you for ${enemyFirePower}`)
+                dmgCalc()
                 if(yourHull<1){
-                    alert('Enemy breached your hull and you are dead')
+                    console.log('Enemy breached your hull and you are dead')
                 }
             }
             else{
-                alert('Enemy missed you')
+                console.log('Enemy missed you')
             }
         }
     }
     if (enemyCount==0){
-        alert('You defeated the aliens and saved Earth')
-        enemyCount=6
-        yourHull=20
+        console.log('You defeated the aliens and saved Earth')
+        reset()
     }
     if (yourHull<1){
-        alert('Aliens Won');
-        yourHull=20
+        console.log('Aliens Won');
+        reset()
     }
     makeEnemy()
     displayStats()
-} 
-button1.addEventListener('click',battleLoop)
-let makeEnemy=()=>{
-    enemyHull=Math.floor(Math.random()*4+3)
-    enemyFirePower=Math.floor(Math.random()*3+2)
-    enemyAccuracy=Math.floor(Math.random()*20+60)/100
 }
-let didYouHit=(accuracy)=>{
-    if (Math.random() < accuracy)
-        return true
+const shieldRegen=()=>{
+    amountRegen=Math.ceil(Math.random()*2*level)
+    yourShield+=amountRegen
+    console.log(`Shield battery activated, Your shield strength increased by ${amountRegen}.`)
+}
+const heal=()=>{
+    if(powerPoints>0){
+    yourHull+=5
+    powerPoints--
+    displayStats()    
+    }
     else
-        return false
+    console.log('No pp lefts')
 }
-let yourStats=document.getElementsByClassName('playerStats')[0]
-let enemyStats=document.getElementsByClassName('enemyStats')[0]
+const atkUp=()=>{
+    if(powerPoints>0){
+    yourFirePower+=1
+    powerPoints--
+    displayStats()    
+    }
+    else
+    console.log('No pp lefts')
+}
+const accUp=()=>{
+    if(powerPoints>0){
+    yourAccuracy+=10
+    powerPoints--
+    displayStats()    
+    }
+    else
+    console.log('No pp lefts')
+}
+const spec=()=>{
+    if(specialAtk>0){
+    console.log('Ima Firing Mah Lazer')
+    specialAtk--
+    enemyHull-=10
+        if(enemyHull<1){
+            console.log('You killed the enemy with your lazer. You get no bonus for that')
+            enemyCount--
+            makeEnemy()
+        }
+    displayStats()
+    }
+    else
+    console.log('No specs lefts')
+}
+const dmgCalc=()=>{
+    if(yourShield>enemyFirePower)
+        yourShield-=enemyFirePower
+    else{
+        yourHull+=yourShield-enemyFirePower
+        yourShield=0 
+    }
+}
+const reset=()=>{
+    yourHull=20
+    yourFirePower=5
+    yourAccuracy=50
+    yourShield=10
+    enemyCount=10
+    level=1
+    powerPoints=0
+    specialAtk=3
+}
+const makeEnemy=()=>{
+    enemyHull=Math.ceil(Math.random()*2*level+3)
+    enemyFirePower=Math.ceil(Math.random()*2*level+2)
+    enemyAccuracy=Math.ceil((.7+(level/100))*100)
+}
+const didYouHit=(accuracy)=>{
+    if (Math.random()*100 < accuracy)
+        return true
+}
 const displayStats=()=>{
-    yourStats.innerHTML=`Hull : ${yourHull} <br>FirePower : 5 <br>Accuracy : .7 <br>`
+    yourStats.innerHTML=`Hull : ${yourHull} <br>FirePower : ${yourFirePower} <br>Accuracy : ${yourAccuracy} <br> Shield: ${yourShield}`
     enemyStats.innerHTML=`Hull : ${enemyHull} <br>FirePower : ${enemyFirePower} <br>Accuracy : ${enemyAccuracy} <br>`
     document.getElementById('p1').innerHTML=`Enemy left=${enemyCount}`
+    document.getElementById('powerPoints').innerHTML=`Power Points left=${powerPoints}`
+    document.getElementById('spAtk').innerHTML=`SpAtk left=${specialAtk}`
+
 }
+function LoadingStatement() {
+    console.log(`Earth has been attacked by a horde of aliens! Time to fight back. Your ship's has ${yourHull} hull strength, ${yourFirePower} firepower and accuracy of ${yourAccuracy}`)
+    console.log(`There is a total of ${enemyCount} enemy and you will fight them in a row`)
+    console.log("Battle Initializing")
+}
+let button=document.getElementsByClassName('button1')
+let yourStats=document.getElementsByClassName('playerStats')[0]
+let enemyStats=document.getElementsByClassName('enemyStats')[0]
+button[0].addEventListener('click',battleLoop)
+button[1].addEventListener('click',heal)
+button[2].addEventListener('click',atkUp)
+button[3].addEventListener('click',accUp)
+button[4].addEventListener('click',spec)
+reset()
 makeEnemy()
 displayStats()
-function LoadingStatement() {
-    alert("Earth has been attacked by a horde of aliens! Time to fight back. Your ship's has 20 hull strength, 5 firepower and hitrate of 70%")
-    alert("There is a total of 6 enemy and you will fight them in a row")
-    alert("Battle Initializing")
-}
-setTimeout(LoadingStatement,1)
+setTimeout(LoadingStatement,100)
